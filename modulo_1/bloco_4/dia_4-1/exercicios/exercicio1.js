fc0c4f15fa21f8078d93b4f266dd78df90e80a82
André Sugai <guitarfreaks@gmail.com>
2022-06-21 20:05:24 -0300

    exercício 1 e 2 de Javascript - Primeiros Passos

---
+++ modulo_1/bloco_4/dia_4-1/exercicios/exercicio1.js
@@ -0,0 +1,22 @@
+// Programa 1
+
+let a = 2;
+let b = 8;
+
+console.log(a + b);
+
+// Programa 2
+
+console.log(a - b);
+
+// Programa 3
+
+console.log(a * b);
+
+// Programa 4
+
+console.log(a / b);
+
+// Programa 5
+
+console.log(a % b);
---
+++ modulo_1/bloco_4/dia_4-1/exercicios/exercicio2.js
@@ -0,0 +1,9 @@
+const a = 10;
+const b = 4;
+
+if (a > b){
+    console.log ('O número a é maior que b')
+}
+else {
+    console.log ('O número b é maior que a')
+}
