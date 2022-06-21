6e342cbdcc96b052e4d6f9c19dd14c760b4b8b39
André Sugai <guitarfreaks@gmail.com>
2022-06-21 16:18:17 -0300

    Bloco 4 e exercícios do dia 4.1

---
+++ modulo_1/bloco_4/dia_4-1/variaveis/script.js
@@ -0,0 +1,9 @@
+const myName = "André";
+const birthCity = "São Paulo";
+
+let birthYear = 1978;
+birthYear = 2030;
+
+console.log(myName + " " +'nasceu em' + " " + birthCity + " " + 'no ano de'+ " " + birthYear);
+
+console.log(birthYear)
