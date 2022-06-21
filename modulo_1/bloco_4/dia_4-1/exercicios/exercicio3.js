dcdc97bca38b58d2ad08d7c2785e844b864a172c
André Sugai <guitarfreaks@gmail.com>
2022-06-21 20:11:38 -0300

    exercício 3 de javascript - primeiros passos

---
+++ modulo_1/bloco_4/dia_4-1/exercicios/exercicio3.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 20;
+const c= 4;
+
+if (a > b && a > c){
+    console.log ('O número a é maior que b e c');
+}
+else if (b > a && b > c){
+    console.log ('O número b é maior que a e c');
+}
+else {
+    console.log ('O número c é maior que a e b');
+}
