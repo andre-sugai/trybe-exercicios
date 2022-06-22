29be700de54b6a34b7b0b5900b0a7aa7a9387d87
André Sugai <guitarfreaks@gmail.com>
2022-06-22 16:19:21 -0300

    exercicios de fixação do dia 4.2

---
+++ modulo_1/bloco_4/dia_4-2/fixando_o_aprendizado/exercicio3.js
@@ -0,0 +1,4 @@
+const lista = ['arroz', 'feijão', 'carne'];
+for (let index = 0; index < lista.length; index+= 1) {
+  console.log(lista[index])
+}
---
+++ modulo_1/bloco_4/dia_4-2/fixando_o_aprendizado/exercicio5.js
@@ -0,0 +1,4 @@
+let animes = ['Dragon Ball', 'Naruto', 'Pokémon'];
+for (let index = 0; index < animes.length; index+= 1) {
+  console.log(animes[index])
+}
