4e728611f492555bf0bedee515000040a6205e22
André Sugai <guitarfreaks@gmail.com>
2022-06-29 16:22:41 -0300

    exercicios do dia 5.2

---
+++ modulo_1/bloco_5/dia_5-2/script2.js
@@ -0,0 +1,23 @@
+// item 1
+console.log(document.getElementById('elementoOndeVoceEsta'));
+
+// item 2
+console.log(document.getElementById('elementoOndeVoceEsta').parentNode.style.color = "red")
+
+// item 3
+console.log(document.getElementById('primeiroFilhoDoFilho').innerText = "Texto dentro do item 3")
+
+// item 4
+console.log(document.getElementById('pai').firstChild)
+
+// item 5
+console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling)
+
+// item 6
+console.log(document.getElementById('elementoOndeVoceEsta').parentElement.innerText)
+
+// item 7
+console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling)
+
+// item 8
+console.log(document.getElementById('pai').lastElementChild.previousElementSibling)
