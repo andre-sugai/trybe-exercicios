afcd92d697a5bed6522d06e723ccc785ec35ba35
André Sugai <guitarfreaks@gmail.com>
2022-07-03 21:12:22 -0300

    administrador de tempo

---
+++ modulo_1/bloco_5/dia_5-1/script.js
@@ -0,0 +1,20 @@
+const corDeFundoHeader = document.getElementById('header-container').style.backgroundColor = 'green';
+
+const destaque1 = document.querySelectorAll('.emergency-tasks div h3')[0].style.backgroundColor = '#a91be9';
+
+const destaque2 = document.querySelectorAll('.emergency-tasks div h3')[1].style.backgroundColor = '#a91be9';
+
+const destaque3 = document.querySelectorAll('.no-emergency-tasks div h3')[0].style.backgroundColor = '#232525';
+
+const destaque4 = document.querySelectorAll('.no-emergency-tasks div h3')[1].style.backgroundColor = '#232525';
+
+const backgroundColor1 = document.querySelectorAll('.emergency-tasks div')[0].style.backgroundColor = '#ff9e88';
+
+const backgroundColor2 = document.querySelectorAll('.emergency-tasks div')[1].style.backgroundColor = '#ff9e88';
+
+const backgroundColor3 = document.querySelectorAll('.no-emergency-tasks div')[0].style.backgroundColor = '#fada72';
+
+const backgroundColor4 = document.querySelectorAll('.no-emergency-tasks div')[1].style.backgroundColor = '#fada72';
+
+const rodape = document.querySelector('#footer-container div').style.backgroundColor = '#003533';
+
